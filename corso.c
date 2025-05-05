25eb47fe6dbc66c2094d33d8e245df670f4a1063
giovanniscarico <scaricogiovanni@gmail.com>
2025-05-05 12:38:48 +0200

    corso.C: Definizione Struttura e CreaCorso

--- corso.c
+++ corso.c
@@ -3,3 +3,29 @@
 //
 
 #include "corso.h"
+
+struct corso{
+    string ID;
+    string nome;
+    Data dataLezione;
+    string oario;
+    int Maxpartecipanti=20;
+    int Numpartecipanti;
+};
+
+Corso CreaCorso(string ID, string nome, Data dataLezione, string orario){
+    Corso co=malloc(sizeof(Corso));
+    if(co==NULL){
+        printf("Errore allocazione memoria\n");
+        exit(1);
+    }
+    strcpy(co->ID,ID);
+    strcpy(co->nome,nome);
+    copiaData(co->dataLezione, dataLezione);
+    strcpy(co->oario,orario);
+    co->Numpartecipanti=0;
+
+
+
+    return co;
+}
